f0bafabfab33f41156da5841372ceddb8ba869f6
Yondergb <Yondergb@gmail.com>
2025-12-27 01:42:55 -0400

    Creando formulario con ventana modal

--- Aplicaciones/Educacion/urls.py
+++ Aplicaciones/Educacion/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio),
+    path('carreras/', views.carreras, name="carreras"),
+    path('crear_carrera/', views.crear_carrera, name="crear_carrera"),
+    path('eliminar_carrera/<id>', views.eliminar_carrera, name="eliminar_carrera"),
 ]
--- Aplicaciones/Educacion/views.py
+++ Aplicaciones/Educacion/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Carrera
 
 # Create your views here.
 def inicio(request):
     return render(request, 'index.html')
-                  
+
+def carreras(request):
+    carrerasDbb = Carrera.objects.all()
+    return render(request, 'carreras.html', {
+        "carreras" : carrerasDbb
+    })
+
+def crear_carrera(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre_car']
+        fecha_creacion = request.POST['fecha_creacion_car']
+        telefono = request.POST['telefono_car']
+
+        Carrera.objects.create(nombre_car=nombre, fecha_creacion_car=fecha_creacion, telefono_car=telefono)
+        return redirect('/carreras')
+
+def eliminar_carrera(request, id):
+    carreraDb = Carrera.objects.get(id_car=id)
+    carreraDb.delete()
+    return redirect('/carreras')
+
+        
